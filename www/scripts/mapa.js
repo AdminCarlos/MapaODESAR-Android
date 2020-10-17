863af0aa5a55c99a6aaf7a35819fcfc9a27f148b
Alejandro Palacios <pajeo97-fap@yahoo.com.mx>
2020-10-16 19:39:17 -0600

    feat: colocar íconos en el mapa dinámicamente

--- www/scripts/mapa.js
+++ www/scripts/mapa.js
@@ -1,10 +1,20 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 let leyendas = null;
+let lugares = null;
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    
-    peticiones()
+    var elem = document.getElementById('divContainerMapa');
+
+    var panzoom = Panzoom(elem, {
+        maxScale: 9,
+        minScale: 3,
+        step: 0.4,
+        startScale: 3,
+        startX: 7.333333333333334,
+        startY: 83.55557250976562
+    });
+    peticiones();
     //mostrar todo cuando cordova esté listo
     document.getElementById('divBody').style.display = "";
 
@@ -20,12 +30,23 @@ function onDeviceReady() {
     document.getElementById('buttonOpenLeyendas').addEventListener('click',(e)=>{
         abrirLeyendas();
     });
+
+    //controles de zoom en el mapa
+    $("#buttonZoom").click(function () {
+        panzoom.zoom(panzoom.getScale() + 0.5);
+    });
+
+    $("#buttonUnzoom").click(function () {
+        panzoom.zoom(panzoom.getScale() - 0.5);
+    });
+
     if (device.platform.toLowerCase == 'browser') {
         //litener de clicks del título de modal de leyendas
         document.getElementById('tituloModalLeyendas').addEventListener('click', (e) => {
             closeLeyendas();
         });
     } else {
+        //listener del botón de atrás
         document.addEventListener("backbutton", onBackKeyDown, false);
 
         function onBackKeyDown() {
@@ -35,14 +56,14 @@ function onDeviceReady() {
             }
         }
     }
-    
 }
 
 //esta función se encargará de hacer las peticiones una sola vez y luego
 //almacenar los resultados en variables para mayor velocidad y poder
 function peticiones() {
-    //petición para los datos de leyendas
+    
     if (device.platform === "Android") {
+        //petición para los datos de leyendas
         $.ajax({
             type: "POST",
             url: "scripts/leyendas.json",
@@ -56,15 +77,39 @@ function peticiones() {
                 console.log("¡¡NO SE PUDO OBTENER LOS DATOS DEL MODAL DE LEYENDAS!!");
             }
         })
+        //peticion para las coordenadas de íconos
+        $.ajax({
+            type: "POST",
+            url: "scripts/lugares.json",
+            data: {},
+            cache: false,
+            success: function (data) {
+                var array = JSON.parse(data);
+                lugares = array.lugares;
+                colocarLugaresEnMapa();
+            },
+            error: function () {
+                console.log("¡¡NO SE PUDO OBTENER LOS DATOS DE LOS LUGARES DEL MAPA!!");
+            }
+        })
     }
     else {
+        //petición para los datos de leyendas
         fetch('scripts/leyendas.json')
             .then((response) => response.json())
             .then(function (array) {
                 leyendas = array.leyendas;
             })
-
+        //peticion de iconos
+        fetch('scripts/lugares.json')
+            .then((response) => response.json())
+            .then(function (array) {
+                lugares = array.lugares;
+                colocarLugaresEnMapa();
+            })
     }
+    
+
 }
 
 function abrirLeyendas() {
@@ -79,4 +124,33 @@ function abrirLeyendas() {
 
 function closeLeyendas() {
     $("#divLeyendas").slideUp();
+}
+
+function colocarLugaresEnMapa() {
+    lugares.forEach(lugar => {
+        //íconos del mapa
+        let icono = document.createElement('img');
+        icono.classList.add('mapIcon');
+        icono.id = lugar.nombre;
+        icono.src = lugar.icono;
+        icono.style.position = "absolute";
+        icono.style.top = ((lugar.coordY / 100) * $("#divContainerMapa").height()) + 'px';
+        icono.style.left = ((lugar.coordX / 100) * $("#divContainerMapa").width()) + 'px';
+        icono.style.width = ((0.4 / 100) * $("#divContainerMapa").width()) + "%";
+        icono.style.zIndex = '100';
+        document.getElementById('divContainerMapa').appendChild(icono);
+
+        //nombres íconos
+        /*  let spanIcono = document.createElement('span');
+         spanIcono.classList.add('spanIcon');
+         spanIcono.style.position = "absolute";
+         spanIcono.style.top = ((lugar.coordY / 100) * $("#divContainerMapa").height()) + 'px';
+         spanIcono.style.left = ((lugar.coordX / 100) * $("#divContainerMapa").width()) + 'px';
+         spanIcono.style.width = + ((0.4 / 100) * $("#divContainerMapa").width()) + "%";
+         spanIcono.style.zIndex = '101';
+         spanIcono.innerHTML = lugar.nombre;
+         document.getElementById('divContainerMapa').appendChild(spanIcono);
+         console.log(((lugar.coordY / 100) * $("#divContainerMapa").height()));
+         console.log(((lugar.coordX / 100) * $("#divContainerMapa").width())); */
+    });
 }
